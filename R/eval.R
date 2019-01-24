a8b5598b42ae977ff7dbb5a51d7483850e530fa5
Alec Robitaille <robit.alec@gmail.com>
2019-01-23 19:02:28 -0800

    fst eval skeleton

---
+++ R/eval.R
@@ -0,0 +1,29 @@
+#' Eval layers by point
+#'
+#' @return
+#' @export
+#'
+#' @examples
+eval_pt <- function(layer, pt, type, direction) {
+
+}
+
+#' Eval layers by point
+#'
+#' @return
+#' @export
+#'
+#' @examples
+eval_buffer <- function(layer, pt, buffersize, type, direction) {
+
+}
+
+#' Eval layers by point
+#'
+#' @return
+#' @export
+#'
+#' @examples
+eval_grid <- function(layer, pt, grid, type, direction) {
+
+}
