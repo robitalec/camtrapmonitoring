8b55a920990e5822dbb27b1023c4a288a042b6b7
Alec Robitaille <robit.alec@gmail.com>
2019-01-23 19:02:19 -0800

    fst camera grid from toast

---
+++ R/make_grid.R
@@ -0,0 +1,15 @@
+#' Make camera grid
+#'
+#' @param DT
+#' @param id
+#' @param case
+#' @param distance
+#' @param coords
+#'
+#' @return
+#' @export
+#'
+#' @examples
+camera_grid <- function(DT, id, case, distance, coords) {
+	# from toast::camera_grid
+}
