f90721ba88cc132d46b8c8ee8b4f9bc1d06d51ee
Alec Robitaille <robit.alec@gmail.com>
2019-02-04 14:03:57 -0800

    add sf test chunk

--- tests/testthat/test-strat-sample.R
+++ tests/testthat/test-strat-sample.R
@@ -5,7 +5,18 @@ data(densitygrid)
 pts <- strat_sample(densitygrid, 10,
 										col = 'density', returnDT = TRUE)
 
-test_that("basic checks", {
+test_that("basic checks with data.table return", {
+	pts <- strat_sample(densitygrid, 10,
+											col = 'density', returnDT = TRUE)
+
+	expect_equivalent(unique(pts$col),
+										unique(densitygrid$density))
+})
+
+test_that("basic checks with sf return", {
+	pts <- strat_sample(densitygrid, 10,
+											col = 'density', returnDT = FALSE)
+
 	expect_equivalent(unique(pts$col),
 										unique(densitygrid$density))
 })
