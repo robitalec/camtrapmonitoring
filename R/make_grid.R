b197a2849d4448722970cdbf22aa06aad617ffb0
Alec Robitaille <robit.alec@gmail.com>
2019-02-04 13:29:57 -0800

    fill cam grid

--- R/make_grid.R
+++ R/make_grid.R
@@ -1,15 +1,42 @@
-#' Make camera grid
+#' Camera grid
 #'
-#' @param DT
-#' @param id
-#' @param case
-#' @param distance
-#' @param coords
+#' Set up a camera grid around a focal point.
+#'
+#' @param DT data.table.
+#' @param id id of focal point.
+#' @param case "queen", "rook" or "bishop".
+#' @param distance distance in x and y between cameras. don't worry about the hypotenuse.
+#' @param coords names of coordinate columns.
 #'
 #' @return
+#'
+#' Extended data.table either nine times the length of input DT for 'queen' case or 5 times the length of input DT for 'rook' or 'bishop' case.
+#'
+#' The logical 'focal' column indicates which point is the focal.
+#'
 #' @export
 #'
 #' @examples
+#' grid <- camera_grid(DT, id = 'point', case = 'queen', distance = 250, coords = c('X', 'Y'))
 camera_grid <- function(DT, id, case, distance, coords) {
-	# from toast::camera_grid
+	# NSE
+	focal <- camX <- camY <- NULL;
+
+	if (case == 'queen') {
+		move <- data.table::CJ(c(-distance, 0, distance),
+													 c(-distance, 0, distance))
+	} else if (case == 'rook') {
+		move <- data.table::CJ(c(-distance, distance),
+													 c(-distance, distance))
+	} else if (case == 'bishop') {
+		move <- data.table::data.table(c(0, distance, 0, -distance),
+																	 c(distance, 0, -distance, 0))
+	}
+
+	DT <- DT[rep(1:.N, times = nrow(move))]
+	DT[, c('camX', 'camY') := .SD + move,
+		 .SDcols = coords,
+		 by = id]
+	DT[camX == get(coords[[1]]) & camY == get(coords[[2]]),
+		 focal := TRUE]
 }
