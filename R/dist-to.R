3bc154c36c232de477d8b493c6550c0f5c2f81cb
Alec Robitaille <robit.alec@gmail.com>
2019-02-08 17:04:55 -0800

    fst dist_to
    from toast

---
+++ R/dist-to.R
@@ -0,0 +1,15 @@
+#' Feature-based distance to
+#'
+#' To avoid the large overhead of creating distance to rasters for small/medium number of sample points, this "vector-based distance to" determines the nearest feature (y) to each x before calculating the distance.
+#'
+#' @param x object of class sfg, sfc or sf
+#' @param y object of class sfg, sfc or sf
+#'
+#' @return Vector of distances between x and the nearest feature in y.
+#' @export
+#'
+#' @examples
+feat_dist_to <- function(x, y) {
+	sf::st_distance(x, y[sf::st_nearest_feature(x, y), ],
+									by_element = TRUE)
+}
