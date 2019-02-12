ec4bec7ae48c3b94f0e5ff6c9f7edc19d0cf16b1
Alec Robitaille <robit.alec@gmail.com>
2019-02-12 12:39:45 -0800

    todo check types dist_to

--- R/dist-to.R
+++ R/dist-to.R
@@ -20,6 +20,8 @@ dist_to <- function(x, y) {
 		stop('please provide both x and y')
 	}
 
+	# check types of x and y
+
 	sf::st_distance(x, y[sf::st_nearest_feature(x, y), ],
 									by_element = TRUE)
 }
