ef71d35b1aef09bf9bf880754a4dd970ffb8b062
Alec Robitaille <robit.alec@gmail.com>
2019-02-15 18:12:22 -0800

    add data.table method

--- R/make_grid.R
+++ R/make_grid.R
@@ -46,16 +46,54 @@ make_grid <- function(x, case, distance, ...) {
 	} else if (case == 'bishop') {
 		move <- data.table::data.table(c(0, distance, 0, -distance),
 																	 c(distance, 0, -distance, 0))
+	} else {
+		stop('must provide case one of "queen", "rook" or "bishop"')
+	}
+
+	UseMethod('make_grid', x)
+}
+
+#' @export
+#' @import data.table
+make_grid.data.table <- function(x, case, distance, id, coords) {
+	# NSE
+	focal <- camX <- camY <- NULL;
+
+	if (!(id %in% colnames(x))) {
+		stop('id provided not found in colnames(x)')
+	}
+
+	if (!(all(coords %in% colnames(x)))) {
+		stop('coords provided not found in colnames(x)')
 	}
 
 	DT <- DT[rep(1:.N, times = nrow(move))]
 	DT[, c('camX', 'camY') := .SD + move,
 		 .SDcols = coords,
 		 by = id]
+
 	DT[camX == get(coords[[1]]) & camY == get(coords[[2]]),
 		 focal := TRUE]
+	DT[is.na(focal), focal := FALSE]
+
+	return(DT)
 }
 
+
+#' @export
+make_grid.sf <- function(x, case, distance) {
+	if (!('sfc_POINT' %in% class(x$geometry))) {
+		stop('class of geometry column must be sfc_POINT')
+	}
+
+	r <- x[rep(1:nrow(x),  each = nrow(move)),]
+
+	out <- sf::st_as_sf(
+		data.frame(r[, colnames(r)[!(grepl('geometry', colnames(r)))]],
+							 st_coordinates(r) +
+							 	as.matrix(move[rep(1:.N, times = nrow(x))])),
+		coords = c('X', 'Y')
+	)
 
 	if (is.null(sf::st_crs(x))) {
 		return(out)
