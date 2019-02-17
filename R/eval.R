0a7ea097244fa2a5ec01300341d58ddd11fdf610
Alec Robitaille <robit.alec@gmail.com>
2019-02-16 18:17:53 -0800

    add first generic methods for eval

--- R/eval.R
+++ R/eval.R
@@ -4,18 +4,38 @@
 #' @export
 #'
 #' @examples
-eval_pt <- function(DT, layer, type, direction) {
-
+eval_pt <- function(x, layer, type, direction, ...) {
+	UseMethod('eval_pt', x)
 }
 
-#' Eval layers by point
+#' @export
+eval_pt.data.table <- function(x, layer, type, direction, coords) {
+	if (length(coords) != 2) {
+		stop('length of coords column names should be 2')
+	}
+
+	# if type numeric coords
+	# if missing x and layer
+	# warn if no type and direction
+
+	raster::extract(layer, x[, data.table::.SD, .SDcols = coords],
+									na.rm = FALSE)
+}
+
+#' @export
+eval_pt.sf <- function(x, layer, type, direction) {
+	raster::extract(layer, sf::st_coordinates(x))
+}
+
+
+#' Eval layers by buffered
 #'
 #' @return
 #' @export
 #'
 #' @examples
 eval_buffer <- function(DT, layer, buffersize, type, direction) {
-
+	extract(buffer = buffersize)
 }
 
 #' Eval layers by point
