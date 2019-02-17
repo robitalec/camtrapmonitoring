0e4e50c15f259624c47a3b3530e9765c968e87cb
Alec Robitaille <robit.alec@gmail.com>
2019-02-16 19:48:37 -0800

    fix check for class RasterLayer

--- R/eval.R
+++ R/eval.R
@@ -4,16 +4,16 @@
 #' @export
 #'
 #' @examples
-eval_pt <- function(x, layer, type, direction, ...) {
+eval_pt <- function(x, layer, type = NULL, direction = NULL, ...) {
 	if (is.null(x)) {
 		stop('x must be provided. either data.table or sf point object.')
 	}
 
-	if (is.null(layer) | !("raster" %in% class(layer))) {
+	if (is.null(layer) || !("RasterLayer" %in% class(layer))) {
 		stop('layer must be provided. expected type is raster.')
 	}
 
-	if (is.null(type) | is.null(direction)) {
+	if (is.null(type) || is.null(direction)) {
 		warning('missing type and/or direction. it is recommended to provide these for subsequent selection of camera trap locations.')
 	}
 
