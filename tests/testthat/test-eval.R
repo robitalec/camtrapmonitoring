83a0f3fc26e25db4312e56098ed8effd93d6ab4e
Alec Robitaille <robit.alec@gmail.com>
2019-02-16 19:00:38 -0800

    add general tests eval_pt

--- tests/testthat/test-eval.R
+++ tests/testthat/test-eval.R
@@ -4,28 +4,49 @@ library(data.table)
 library(sf)
 
 data(points)
+data(lc)
 
 DT <-
 	data.table(ID = points$ID,
 						 st_coordinates(points))
 
-
+###
+expect_error(
+	eval_pt(x = DT, layer = lc,
+					type = 'categorical', direction = 'neutral',
+					coords = c('X', 'Y')),
+)
+###
 
 test_that("eval_pt, general", {
 	expect_error(
-		make_grid(points, case = 'potato', distance = 100),
-		'must provide case one of "queen", "rook" or "bishop"'
+		eval_pt(x = NULL, layer = lc,
+						type = 'categorical', direction = 'neutral',
+						coords = c('X', 'Y')),
+		'x must be provided. either data.table or sf point object.'
 	)
 
 	expect_error(
-		make_grid(points, case = 'queen', distance = -100),
+		eval_pt(x = DT, layer = NULL,
+						type = 'categorical', direction = 'neutral',
+						coords = c('X', 'Y')),
 		'layer must be provided. expected type is raster.'
 	)
 
-	expect_error(
-		make_grid(1, case = 'queen', distance = 100),
-		'no applicable method for ', fixed = FALSE
+	expect_warning(
+		eval_pt(x = DT, layer = NULL,
+						type = NULL, direction = 'neutral',
+						coords = c('X', 'Y')),
+		'missing type and/or direction', fixed = FALSE
 	)
+
+	expect_warning(
+		eval_pt(x = DT, layer = NULL,
+						type = 'categorical', direction = NULL,
+						coords = c('X', 'Y')),
+		'missing type and/or direction', fixed = FALSE
+	)
+
 })
 
 test_that("eval_pt, data.table", {
