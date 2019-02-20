12bb5b6aa838e9f47eef315b1872f1bf150862d7
Alec Robitaille <robit.alec@gmail.com>
2019-02-19 16:01:31 -0800

    add check coords eval_pt

--- R/eval.R
+++ R/eval.R
@@ -17,8 +17,10 @@ eval_pt <- function(x, layer, type = NULL, direction = NULL, ...) {
 		warning('missing type and/or direction. it is recommended to provide these for subsequent selection of camera trap locations.')
 	}
 
+	# if missing x and layer
 	# if type isn't one of
 	# if direction isn't one of
+	# warn if no direction or no type
 
 	nm <- deparse(substitute(layer))
 
@@ -31,9 +33,9 @@ eval_pt.data.table <- function(x, layer, type, direction, coords) {
 		stop('length of coords column names should be 2')
 	}
 
-	# if type numeric coords
-	# if missing x and layer
-	# warn if no type and direction
+	if (!all(sapply(DT[, .SD, .SDcols = coords], is.numeric))) {
+		stop('coords provided must be numeric')
+	}
 
 	raster::extract(layer, x[, .SD, .SDcols = coords],
 									na.rm = FALSE)
@@ -55,14 +57,4 @@ eval_pt.sf <- function(x, layer, type, direction) {
 #' @examples
 eval_buffer <- function(DT, layer, buffersize, type, direction) {
 	# extract(buffer = buffersize)
-}
-
-#' Eval layers by point
-#'
-#' @return
-#' @export
-#'
-#' @examples
-eval_grid <- function(DT, layer, grid, type, direction) {
-	# build grid or expect grid?
 }
