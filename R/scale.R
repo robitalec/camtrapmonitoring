34ebb3fcad8291794f28402cd6cc604d0fcbcaad
Alec Robitaille <robit.alec@gmail.com>
2019-02-21 17:10:05 -0800

    add skeleton

--- R/scale.R
+++ R/scale.R
@@ -1,12 +1,14 @@
 #' Scale in region of interest
 #'
+#' Scale a layer in a region of interest to optionally compare locations with [eval_pt()] and [eval_buffer()], and select locations with [select_ct()] based off of relative values instead of absolute values.
+#'
 #' @param layer
-#' @param roi
+#' @param roi sf
 #'
 #' @return
 #' @export
 #'
 #' @examples
 scale_roi <- function(layer, roi) {
-
+	raster::scale(raster::crop(layer, roi))
 }
