d7f6ac08d527e85d357f0a7a2f1bdabbef908950
Alec Robitaille <robit.alec@gmail.com>
2019-02-22 17:00:05 -0800

    tidy eval tests

--- tests/testthat/test-eval.R
+++ tests/testthat/test-eval.R
@@ -1,65 +1,43 @@
 context("test-eval")
 
+evalDT <- eval_pt(DT, layer = lc, type = 'categorical',
+									direction = 'neutral', coords = c('X', 'Y'))
+
 test_that("eval_pt, general", {
 	expect_error(
 		eval_pt(x = NULL, layer = lc,
-						type = 'categorical', direction = 'neutral',
-						coords = c('X', 'Y')),
+						type = 'categorical', direction = 'neutral'),
 		'x must be provided. either data.table or sf point object.'
 	)
 
 	expect_error(
-		eval_pt(x = DT, layer = NULL,
-						type = 'categorical', direction = 'neutral',
-						coords = c('X', 'Y')),
+		eval_pt(x = points, layer = NULL,
+						type = 'categorical', direction = 'neutral'),
 		'layer must be provided. expected type is raster.'
 	)
 
 	expect_warning(
-		eval_pt(x = DT, layer = lc,
-						type = NULL, direction = 'neutral',
-						coords = c('X', 'Y')),
+		eval_pt(x = points, layer = lc,
+						type = NULL, direction = 'neutral'),
 		'missing type and/or direction', fixed = FALSE
 	)
 
 	expect_warning(
-		eval_pt(x = DT, layer = lc,
-						type = 'categorical', direction = NULL,
-						coords = c('X', 'Y')),
+		eval_pt(x = points, layer = lc,
+						type = 'categorical', direction = NULL),
 		'missing type and/or direction', fixed = FALSE
 	)
 
 	expect_error(
-		eval_pt(x = DT, layer = lc,
-						type = 'categorical', direction = 42,
-						coords = c('X', 'Y')),
+		eval_pt(x = points, layer = lc,
+						type = 'categorical', direction = 42),
 		'type and direction must be of class character'
 	)
 
 	expect_error(
-		eval_pt(x = DT, layer = lc,
-						type = 42, direction = 'positive',
-						coords = c('X', 'Y')),
+		eval_pt(x = points, layer = lc,
+						type = 42, direction = 'positive'),
 		'type and direction must be of class character'
-	)
-
-})
-
-test_that("eval_pt, data.table", {
-
-	expect_error(
-		eval_pt(x = DT, layer = lc, type = 'categorical',
-						direction = 'neutral', coords = 'X'),
-		'length of coords column names should be 2'
-	)
-
-
-	DT[, xchr := 'potato']
-
-	expect_error(
-		eval_pt(x = DT, layer = lc, type = 'categorical',
-						direction = 'neutral', coords = c('xchr', 'xchr')),
-		'coords provided must be numeric'
 	)
 
 })
@@ -129,7 +107,6 @@ test_that("eval_buffer, general", {
 
 
 test_that("eval_buffer, data.table", {
-
 	expect_error(
 		eval_buffer(x = DT, layer = lc, buffersize = 50,
 								type = 'categorical', direction = 'neutral',
