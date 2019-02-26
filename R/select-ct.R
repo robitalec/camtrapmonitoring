3a3b0ede99256ccf2523a897bb097c6cc717ced6
Alec Robitaille <robit.alec@gmail.com>
2019-02-26 10:33:14 -0800

    add sf/data.table return

--- R/select-ct.R
+++ R/select-ct.R
@@ -15,26 +15,42 @@
 #' @export
 #'
 #' @examples
-select_ct <- function(x, n, rank, sub, by) {
-	# sub is a named list e.g.: list(lc = 212)
-	# rank is a vector of column names
-	# by is a vector of column names
-
-
+select_ct <- function(x, n, rank, sub, by = NULL) {
 	# NAs detected, removing
 	# na.omit
 
+	# should sub do 'lc > 202'?
 
+	# should rank do: camera grid -> summary stat?
 
 	directions <- vapply(rank, function(col) parse_directions(x, col), 1L)
 
 
-	### x[sub][order(rank)][, .SD[1:n], by] ###
-	data.table::setorderv(
-		x[sub, on = names(sub)],
-		cols = c(by, names(directions)),
-		order = c(rep(1, length(by)), directions)
-	)[, .SD[1:n], by]
+	if (inherits(x, 'sf')) {
+		t <- 'sf'
+		x <- as.data.table(x)
+	} else if (inherits(x, 'data.table')) {
+		t <- 'dt'
+	} else {
+		stop('x must be either a data.table or an sf object')
+	}
+
+	nby <- x[, .GRP, by = by][, .N]
+
+
+	sel <- function(x) {
+		data.table::setorderv(
+			x[sub, on = names(sub)],
+			cols = c(by, names(directions)),
+			order = c(rep(1, length(by)), directions)
+		)[, .SD[1:n / nby], by]
+	}
+
+	if (t == 'dt') {
+		return(sel(x))
+	} else if (t == 'sf') {
+		return(sf::st_as_sf(sel(x)))
+	}
 }
 
 
