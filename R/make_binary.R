3b55857be368fc02ca9cf60f17dd8d5a46603f42
Alec Robitaille <robit.alec@gmail.com>
2019-02-27 19:10:49 -0800

    add man skeleton and basic type checks

--- R/make_binary.R
+++ R/make_binary.R
@@ -1,3 +1,32 @@
-make_binary <- function(r, val) {
-	is.na(raster::mask(r, r, maskvalue = val))
+#' Binary rasters
+#'
+#' Make a binary raster from input 'layer'.
+#'
+#' @param layer
+#' @param value
+#'
+#' @return
+#'
+#' A binary raster layer with two values:
+#'
+#' @export
+#'
+#' @examples
+make_binary <- function(layer, value) {
+	if (missing(layer)) {
+		stop('layer must be provided.')
+	}
+
+	if (missing(value)) {
+		stop('value must be provided.')
+	}
+
+	if (!inherits(layer, 'Raster')) {
+		stop('layer must be a raster.')
+	}
+
+	if (!inherits(value, 'numeric')) {
+		stop('value must be a numeric')
+	}
+	is.na(raster::mask(layer, layer, maskvalue = value))
 }
