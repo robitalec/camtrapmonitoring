1ad42c426f96a91693301a2eae09fe9347d5c1f6
Alec Robitaille <robit.alec@gmail.com>
2019-03-01 12:31:18 -0800

    add test that original NAs remain after make_grid

--- tests/testthat/test-make-binary.R
+++ tests/testthat/test-make-binary.R
@@ -12,7 +12,6 @@ test_that("make_binary works", {
 
 	expect_equal(res(lc), res(lc212))
 
-
 	expect_error(make_binary(),
 							 'layer must be provided.')
 
@@ -28,4 +27,14 @@ test_that("make_binary works", {
 	expect_error(make_binary(lc, c(212, 212)),
 							 'value must be of length one.')
 
+})
+
+
+test_that('make_binary handles NA', {
+	buf <- st_buffer(points[1,], 1e2)
+	mlc <- mask(crop(lc, buf), buf, inverse = TRUE)
+
+	expect_true(NA %in% raster::unique(mlc, na.last = TRUE))
+
+	expect_true(NA %in% raster::unique(make_binary(mlc, 212), na.last = TRUE))
 })
