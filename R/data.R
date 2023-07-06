0489b96a52cf4b073b1122eb1a8b7321f69e5825
Alec L. Robitaille <robit.alec@gmail.com>
2023-07-06 08:51:15 -0300

    add source

--- R/data.R
+++ R/data.R
@@ -2,11 +2,11 @@
 #'
 #' A dataset containing roads nearby Rocky Lake, Manitoba.
 #'
-#' Roads are from the National Road Network with all corresponding fields.
+#' Roads are from the Canadian National Road Network (NRN) with all corresponding fields.
 #'
-#' @format An `sf` object with 172 roads and 48 variables.
+#' @format An `sf` object with 172 roads and 48 variables. See the source below for details.
 #'
-#'
+#' @source <https://open.canada.ca/data/en/dataset/3d282116-e556-400c-9306-ca1a3cada77f>
 #' @examples
 #' data(rocky_lake_roads)
 #' plot(rocky_lake_roads)
