1280884886059e8e4bcfa2b8a96c7f9dd61a6cbe
Alec L. Robitaille <robit.alec@gmail.com>
2023-07-25 17:09:45 -0300

    rm check coords

--- R/internal.R
+++ R/internal.R
@@ -30,22 +30,3 @@ check_direction <- function(direction) {
 		}
 	}
 }
-
-
-check_coords <- function(x, coords) {
-	if (is.null(coords)) {
-		stop('coords must be provided if x is a data.table')
-	}
-
-	if (length(coords) != 2) {
-		stop('coords must be a character vector of length 2')
-	}
-
-	if (any(!(coords %in% colnames(x)))) {
-		stop('coords columns not found in x')
-	}
-
-	if (!all(vapply(x[, .SD, .SDcols = coords], is.numeric, TRUE))) {
-		stop('coords provided must be numeric')
-	}
-}
